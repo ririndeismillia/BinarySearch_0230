78e43ddc1d71a441a64b5c874e0208d09e9e0c00
ririndeismillia <ririndeismillia@gmail.com>
2026-04-14 10:38:33 +0700

    void input3

--- binarySerach.cpp
+++ binarySerach.cpp
@@ -21,4 +21,13 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
+    cout << "\==============================================\n";
+    cout << "          Masukkan Eleman Array                 \n";
+    cout << "=================================================\n";
+
+    for (int i = 0; i <nPanjang; i++)
+    {
+        cout << "Data ke-" << (1 + 1)<< " = ";
+        cin >> element[i];
+    }
 }
